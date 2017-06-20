98dcdf2d5a52b34c14c84831635bfb85102fdcc5
Tjark Weber <1020489+tjark@users.noreply.github.com>
2017-06-20 16:36:20 +0000

    Remove script that finds bit-vector division and related operations

--- tools/scripts/detect-bv-div.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#
-# finds all benchmarks files in the current directory that contain a division
-# operation on bit-vectors and prints their path to stdout
-#
-
-egrep -r -l 'bvudiv|bvurem|bvsdiv|bvsrem|bvsmod' .
