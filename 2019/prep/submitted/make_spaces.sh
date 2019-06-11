8bc2e7b5d66f18750c94273de6537878e8c30392
Aina Niemetz <aina.niemetz@gmail.com>
2019-06-10 17:44:44 -0700

    Fix

--- 2019/prep/submitted/make_spaces.sh
+++ 2019/prep/submitted/make_spaces.sh
@@ -7,7 +7,7 @@ PREPARE="$SCRIPTDIR/../../../tools/prep/prepare_space_xml.py"
 IN_SPACE_SQ="../non-incremental-space.xml"
 BENCHMARKS_SQ="../SMT-LIB_non_incremental_benchmarks_all_no_challenge.txt"
 SOLVERS_CSV="$SCRIPTDIR/../../registration/solvers_divisions_final.csv"
-SELECT_SQ="$SCRIPTDIR/../selection/single_query/single_query_2019_no_strings"
+SELECT_SQ="$SCRIPTDIR/../selection/single_query/benchmark_selection_single_query_2019_no_strings"
 Z3_SQ=24192
 
 python $PREPARE "$IN_SPACE_SQ" "$SOLVERS_CSV" "$OUT_SPACE_SQ" -t single_query --select "$SELECT_SQ" -w -e $Z3_SQ
