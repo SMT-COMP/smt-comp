a041d1832891a8d5f7ff59463e0f4c0198591bd1
Aina Niemetz <aina.niemetz@gmail.com>
2019-06-10 18:04:57 -0700

    Add challenge incremental competition and benchmark spaces.

--- 2019/prep/submitted/make_spaces.sh
+++ 2019/prep/submitted/make_spaces.sh
@@ -2,6 +2,7 @@
 
 OUT_SPACE_SQ=$1
 OUT_SPACE_CHALL_NON_INC=$2
+OUT_SPACE_CHALL_INC=$3
 
 SCRIPTDIR=`dirname $(readlink -f "$0")`
 
@@ -14,9 +15,18 @@ SELECT_SQ="$SCRIPTDIR/../selection/single_query/benchmark_selection_single_query
 IN_SPACE_CHALL_NON_INC="../non-incremental-space.xml"
 SELECT_CHALL_NON_INC="$SCRIPTDIR/../selection/challenge-non-incremental/benchmark_selection_challenge_non-incremental_2019"
 
+IN_SPACE_CHALL_INC="../incremental-space.xml"
+SELECT_CHALL_INC="$SCRIPTDIR/../selection/challenge-incremental/benchmark_selection_challenge_incremental_2019"
+
 Z3_SQ=24192
 Z3_CHALL_NON_INC=24192
+Z3_CHALL_INC=24017
 
+# Single Query Track
 python $PREPARE "$IN_SPACE_SQ" "$SOLVERS_CSV" "$OUT_SPACE_SQ" -t single_query --select "$SELECT_SQ" -w -e $Z3_SQ
 
+# Challenge Track non-incremental
 python $PREPARE "$IN_SPACE_CHALL_NON_INC" "$SOLVERS_CSV" "$OUT_SPACE_CHALL_NON_INC" -t single_query_challenge --select "$SELECT_CHALL_NON_INC" -w -e $Z3_CHALL_NON_INC
+
+# Challenge Track incremental
+python $PREPARE "$IN_SPACE_CHALL_INC" "$SOLVERS_CSV" "$OUT_SPACE_CHALL_INC" -t single_query_challenge --select "$SELECT_CHALL_INC" -w -e $Z3_CHALL_INC
