5a1f6f3a8bc131762540ec86b9a7547beee8a070
Aina Niemetz <aina.niemetz@gmail.com>
2019-06-12 22:36:10 -0700

    Refactor make_selection.sh to be more user friendly.

--- 2019/prep/submitted/make_spaces.sh
+++ 2019/prep/submitted/make_spaces.sh
@@ -1,13 +1,5 @@
 #! /bin/bash
 
-OUT_SPACE_SQ=$1
-OUT_SPACE_CHALL_NON_INC=$2
-OUT_SPACE_CHALL_INC=$3
-OUT_SPACE_INC=$4
-OUT_SPACE_MV=$5
-OUT_SPACE_SQ_ONLY=$6
-OUT_SPACE_CHALL_NON_INC_ONLY=$7
-
 SCRIPTDIR=`dirname $(readlink -f "$0")`
 
 PREPARE="$SCRIPTDIR/../../../tools/prep/prepare_space_xml.py"
@@ -35,23 +27,101 @@ NON_COMPETITIVE_INC="24017,23970"       # Z3,Boolector-ReasonLS
 
 ONLY_SOLVERS_SQ="24281,24282"           # STP-mergesat-fixed,STP-portfolio-fixed
 
+OUT_SPACE_SQ=
+OUT_SPACE_CHALL_NON_INC=
+OUT_SPACE_CHALL_INC=
+OUT_SPACE_INC=
+OUT_SPACE_MV=
+OUT_SPACE_SQ_FIXED=
+OUT_SPACE_CHALL_NON_INC_FIXED=
+
+while [ $# -gt 0 ]
+do
+  case $1 in
+    -h|--help)
+      echo -n "usage: $(basename $0) [<option>]"
+      echo
+      echo "Generate StarExec competition spaces."
+      echo
+      echo "  options:"
+      echo "    -h, --help          print this message and exit"
+      echo "    --sq <file>         Single Query track output xml"
+      echo "    --inc <file>        Incremental track output xml"
+      echo "    --csq <file>        Challenge track non-incremental output xml"
+      echo "    --cinc <file>       Challenge track incremental output xml"
+      echo "    --uc <file>         Unsat Core track output xml"
+      echo "    --mv <file>         Model Validation track output xml"
+      echo "    --sq-fixed <file>   Single Query track output xml for fixed"
+      echo "                        (non-competing) solvers"
+      echo "    --csq-fixed <file>  Challenge track non-incremental output xml"
+      echo "                        for fixed (non-competing) solvers"
+      echo
+      exit
+      ;;
+    --sq)
+      shift
+      OUT_SPACE_SQ=$1
+      ;;
+    --inc)
+      shift
+      OUT_SPACE_INC=$1
+      ;;
+    --csq)
+      shift
+      OUT_SPACE_CHALL_NON_INC=$1
+      ;;
+    --cinc)
+      shift
+      OUT_SPACE_CHALL_INC=$1
+      ;;
+    --uc)
+      ;;
+    --mv)
+      shift
+      OUT_SPACE_MV=$1
+      ;;
+    --sq-fixed)
+      shift
+      OUT_SPACE_SQ_FIXED=$1
+      ;;
+    --csq-fixed)
+      shift
+      OUT_SPACE_CHALL_NON_INC_FIXED=$1
+      ;;
+    -*)
+        echo "ERROR: invalid option '$1'"
+        exit 1
+      ;;
+    *)
+      break
+  esac
+  shift
+done
+
 # Single Query Track
+[[ -n $OUT_SPACE_SQ ]] && \
 python $PREPARE "$IN_SPACE_SQ" "$SOLVERS_CSV" "$OUT_SPACE_SQ" -t single_query --select "$SELECT_SQ" -w -e $NON_COMPETITIVE_SQ
 
 # Challenge Track non-incremental
+[[ -n $OUT_SPACE_CHALL_NON_INC ]] && \
 python $PREPARE "$IN_SPACE_CHALL_NON_INC" "$SOLVERS_CSV" "$OUT_SPACE_CHALL_NON_INC" -t single_query_challenge --select "$SELECT_CHALL_NON_INC" -w -e $NON_COMPETITIVE_CHALL_NON_INC
 
 # Challenge Track incremental
+[[ -n $OUT_SPACE_CHALL_INC ]] && \
 python $PREPARE "$IN_SPACE_CHALL_INC" "$SOLVERS_CSV" "$OUT_SPACE_CHALL_INC" -t incremental_challenge --select "$SELECT_CHALL_INC" -w -e $NON_COMPETITIVE_CHALL_INC
 
 # Incremental Track
+[[ -n $OUT_SPACE_INC ]] && \
 python $PREPARE "$IN_SPACE_INC" "$SOLVERS_CSV" "$OUT_SPACE_INC" -t incremental --select "$SELECT_INC" -w -e $NON_COMPETITIVE_INC
 
-# Single Query Track
+# Model Validation Track
+[[ -n $OUT_SPACE_MV ]] && \
 python $PREPARE "$IN_SPACE_MV" "$SOLVERS_CSV" "$OUT_SPACE_MV" -t model_validation --select "$SELECT_MV" -w
 
 # Single Query Track - fixed STP versions
-python $PREPARE "$IN_SPACE_SQ" "$SOLVERS_CSV" "$OUT_SPACE_SQ_ONLY" -t single_query --select "$SELECT_SQ" -w -e $NON_COMPETITIVE_SQ --only-solvers $ONLY_SOLVERS_SQ
+[[ -n $OUT_SPACE_SQ_FIXED ]] && \
+python $PREPARE "$IN_SPACE_SQ" "$SOLVERS_CSV" "$OUT_SPACE_SQ_FIXED" -t single_query --select "$SELECT_SQ" -w -e $NON_COMPETITIVE_SQ --only-solvers $ONLY_SOLVERS_SQ
 
 # Challenge Track non-incremental - fixed STP versions
-python $PREPARE "$IN_SPACE_CHALL_NON_INC" "$SOLVERS_CSV" "$OUT_SPACE_CHALL_NON_INC_ONLY" -t single_query_challenge --select "$SELECT_CHALL_NON_INC" -w -e $NON_COMPETITIVE_CHALL_NON_INC --only-solvers $ONLY_SOLVERS_SQ
+[[ -n $OUT_SPACE_CHALL_NON_INC_FIXED ]] && \
+python $PREPARE "$IN_SPACE_CHALL_NON_INC" "$SOLVERS_CSV" "$OUT_SPACE_CHALL_NON_INC_FIXED" -t single_query_challenge --select "$SELECT_CHALL_NON_INC" -w -e $NON_COMPETITIVE_CHALL_NON_INC --only-solvers $ONLY_SOLVERS_SQ
