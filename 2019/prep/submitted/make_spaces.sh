f8bd8ed5679e1d7bd867a0a64672948c5040a666
Aina Niemetz <aina.niemetz@gmail.com>
2019-06-12 23:27:26 -0700

    Add UC competition and benchmarks space.

--- 2019/prep/submitted/make_spaces.sh
+++ 2019/prep/submitted/make_spaces.sh
@@ -5,8 +5,11 @@ SCRIPTDIR=`dirname $(readlink -f "$0")`
 PREPARE="$SCRIPTDIR/../../../tools/prep/prepare_space_xml.py"
 SOLVERS_CSV="$SCRIPTDIR/../../registration/solvers_divisions_final.csv"
 
-IN_SPACE_MV="../non-incremental-space.xml"
-SELECT_MV="$SCRIPTDIR/../selection/model_validation/benchmark_selection_model_validation_2019"
+IN_SPACE_SQ="../non-incremental-space.xml"
+SELECT_SQ="$SCRIPTDIR/../selection/single_query/benchmark_selection_single_query_2019_no_strings"
+
+IN_SPACE_INC="../incremental-space.xml"
+SELECT_INC="$SCRIPTDIR/../selection/incremental/benchmark_selection_incremental_2019"
 
 IN_SPACE_CHALL_NON_INC="../non-incremental-space.xml"
 SELECT_CHALL_NON_INC="$SCRIPTDIR/../selection/challenge-non-incremental/benchmark_selection_challenge_non-incremental_2019"
@@ -14,16 +17,17 @@ SELECT_CHALL_NON_INC="$SCRIPTDIR/../selection/challenge-non-incremental/benchmar
 IN_SPACE_CHALL_INC="../incremental-space.xml"
 SELECT_CHALL_INC="$SCRIPTDIR/../selection/challenge-incremental/benchmark_selection_challenge_incremental_2019"
 
-IN_SPACE_SQ="../non-incremental-space.xml"
-SELECT_SQ="$SCRIPTDIR/../selection/single_query/benchmark_selection_single_query_2019_no_strings"
+IN_SPACE_UC="../non-incremental-space.xml"
+SELECT_UC="$SCRIPTDIR/../selection/unsat_core/benchmark_selection_unsat_core_2019"
 
-IN_SPACE_INC="../incremental-space.xml"
-SELECT_INC="$SCRIPTDIR/../selection/incremental/benchmark_selection_incremental_2019"
+IN_SPACE_MV="../non-incremental-space.xml"
+SELECT_MV="$SCRIPTDIR/../selection/model_validation/benchmark_selection_model_validation_2019"
 
 NON_COMPETITIVE_SOLVERS_SQ="24192,24193,24160"  # Z3,Boolector-ReasonLS,CVC4-SymBreak
 NON_COMPETITIVE_SOLVERS_CHALL_NON_INC="24192"   # Z3
 NON_COMPETITIVE_SOLVERS_CHALL_INC="24017"       # Z3
 NON_COMPETITIVE_SOLVERS_INC="24017,23970"       # Z3,Boolector-ReasonLS
+NON_COMPETITIVE_SOLVERS_UC="24202"              # Z3
 
 FIXED_SOLVERS_SQ="24281,24282"          # STP-mergesat-fixed,STP-portfolio-fixed
 
@@ -31,6 +35,7 @@ OUT_SPACE_SQ=
 OUT_SPACE_CHALL_NON_INC=
 OUT_SPACE_CHALL_INC=
 OUT_SPACE_INC=
+OUT_SPACE_UC=
 OUT_SPACE_MV=
 OUT_SPACE_SQ_FIXED=
 OUT_SPACE_CHALL_NON_INC_FIXED=
@@ -75,6 +80,8 @@ do
       OUT_SPACE_CHALL_INC=$1
       ;;
     --uc)
+      shift
+      OUT_SPACE_UC=$1
       ;;
     --mv)
       shift
@@ -113,6 +120,10 @@ python $PREPARE "$IN_SPACE_CHALL_INC" "$SOLVERS_CSV" "$OUT_SPACE_CHALL_INC" -t i
 # Incremental Track
 [[ -n $OUT_SPACE_INC ]] && \
 python $PREPARE "$IN_SPACE_INC" "$SOLVERS_CSV" "$OUT_SPACE_INC" -t incremental --select "$SELECT_INC" -w --nc $NON_COMPETITIVE_SOLVERS_INC
+
+# Unsat Core Track
+[[ -n $OUT_SPACE_UC ]] && \
+python $PREPARE "$IN_SPACE_UC" "$SOLVERS_CSV" "$OUT_SPACE_UC" -t unsat_core --select "$SELECT_UC" -w --nc $NON_COMPETITIVE_SOLVERS_UC
 
 # Model Validation Track
 [[ -n $OUT_SPACE_MV ]] && \
