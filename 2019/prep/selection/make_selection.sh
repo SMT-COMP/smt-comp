55fab314e32ef3c90fa0a1c826d622b397abfb2b
Aina Niemetz <aina.niemetz@gmail.com>
2019-06-12 22:14:10 -0700

    Add benchmark selection for unsat core track.

--- 2019/prep/selection/make_selection.sh
+++ 2019/prep/selection/make_selection.sh
@@ -3,23 +3,29 @@
 OUT_SQ=$1
 OUT_INC=$2
 OUT_MV=$3
+OUT_UC=$4
 
 SCRIPTDIR=`dirname $(readlink -f "$0")`
 SELECT="$SCRIPTDIR/../../../tools/selection/selection.py"
 
 BENCHMARKS_SQ="$SCRIPTDIR/../SMT-LIB_non_incremental_benchmarks_all_no_challenge.txt"
 NEW_SQ_CSV="$SCRIPTDIR/../SMT-LIB_non_incremental_benchmarks_new_no_challenge.txt"
-OLD_SQ_CSV="$SCRIPTDIR/../../../2018/csv/Main_Track.csv"
+FILTER_SQ_CSV="$SCRIPTDIR/../../../2018/csv/Main_Track.csv"
 
 BENCHMARKS_INC="$SCRIPTDIR/../SMT-LIB_incremental_benchmarks_all_no_challenge.txt"
 NEW_INC_CSV="$SCRIPTDIR/../SMT-LIB_incremental_benchmarks_new_no_challenge.txt"
-OLD_INC_CSV="$SCRIPTDIR/../../../2018/csv/Application_Track.csv"
+FILTER_INC_CSV="$SCRIPTDIR/../../../2018/csv/Application_Track.csv"
+
+BENCHMARKS_UC="$SCRIPTDIR/../SMT-LIB_non_incremental_benchmarks_unsat.txt"
+NEW_UC_CSV="$SCRIPTDIR/../SMT-LIB_non_incremental_benchmarks_new_unsat.txt"
+FILTER_UC_CSV="$SCRIPTDIR/../SMT-LIB_non_incremental_benchmarks_status_asserts.csv"
 
 BENCHMARKS_MV="$SCRIPTDIR/../SMT-LIB_non_incremental_benchmarks_sat.txt"
 NEW_MV_CSV="$SCRIPTDIR/../SMT-LIB_non_incremental_benchmarks_new_sat.txt"
 
 SEED=912681576
 
-#python $SELECT --filter "$OLD_SQ_CSV" --benchmarks "$BENCHMARKS_SQ" --new-benchmarks "$NEW_SQ_CSV" -s $SEED --print-stats --out "$OUT_SQ" --prefix "/non-incremental/"
-#python $SELECT --filter "$OLD_INC_CSV" --benchmarks "$BENCHMARKS_INC" --new-benchmarks "$NEW_INC_CSV" -s $SEED --print-stats --out "$OUT_INC" --prefix "/incremental/"
+python $SELECT --filter "$FILTER_SQ_CSV" --benchmarks "$BENCHMARKS_SQ" --new-benchmarks "$NEW_SQ_CSV" -s $SEED --print-stats --out "$OUT_SQ" --prefix "/non-incremental/"
+python $SELECT --filter "$FILTER_INC_CSV" --benchmarks "$BENCHMARKS_INC" --new-benchmarks "$NEW_INC_CSV" -s $SEED --print-stats --out "$OUT_INC" --prefix "/incremental/"
 python $SELECT --benchmarks "$BENCHMARKS_MV" --new-benchmarks "$NEW_MV_CSV" -s $SEED --print-stats --out "$OUT_MV" --prefix "/non-incremental/"
+python $SELECT --filter "$FILTER_UC_CSV" --benchmarks "$BENCHMARKS_UC" --new-benchmarks "$NEW_UC_CSV" -s $SEED --print-stats --out "$OUT_UC" --prefix "/non-incremental/" --unsat
