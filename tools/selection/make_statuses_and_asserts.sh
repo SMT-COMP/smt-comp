16d89d309287a3685a5932e2a0bb976c9652670a
Jochen Hoenicke <hoenicke@gmail.com>
2020-05-12 17:46:14 +0200

    Added script for statuses and assert count csv

---
+++ tools/selection/make_statuses_and_asserts.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ "$#" != "2" ]; then
+    echo "USAGE: ./make_status_asserts.sh <scrambler> <smtlib dir>"
+    echo "  <scrambler>:  path to scrambler binary, compiled from "
+    echo "                https://github.com/smt-comp/scrambler"
+    echo "  <smtlib dir>: path to smtlib directory (either incremental or non-incremental"
+    exit
+fi
+
+SCRAMBLER=$1
+SMTLIB_DIR=$2
+
+echo "benchmark,number of asserts,status"
+cd $SMTLIB_DIR
+find . -name '*.smt2' | while read file; do
+   n_asrts=$($SCRAMBLER -count-asserts true < "$file" 2>&1 >/dev/null | \
+             sed -n 's/^; Number of assertions: \([0-9][0-9]*\)/\1/p')
+   status=$(grep '([ \t]*set-info[ \t][ \t]*:status[ \t].*[ \t]*)' < "$file" | \
+	    sed -n 's/.*[ \t]\(unsat\|sat\|unknown\).*/\1/p')
+   echo "$file,$n_asrts,$status"
+done
