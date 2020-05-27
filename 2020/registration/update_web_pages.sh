781f598f611650766356e17b1410791ce6848395
Jochen Hoenicke <hoenicke@gmail.com>
2020-05-27 16:10:35 +0200

    Update Final Solver IDs (#26)
    * Update solver Final Ids and change tracks as requested.
    * remove id of wrapped incremental solver
    * Added small script to update web-pages from csv
    * Set seed to zero for non-competing solver
    * Added non-competing z3.
    * Added NYSE index

---
+++ 2020/registration/update_web_pages.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Update the web pages from the solvers_division_final.csv.
+# You should have checked out the smt-comp.github.io repository into the
+# same directory as smt-comp
+
+NYSE_ON_MAY27="11787.5646"
+
+../../tools/prep/extract_data_from_solvers_divisions.py solvers_divisions_final.csv ../../../smt-comp.github.io/_participants_2020 2020
+../../tools/prep/make_participants_md.py -y 2020 -n "2020-05-27;$NYSE_ON_MAY27" ../../../smt-comp.github.io/2020/
