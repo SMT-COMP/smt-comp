01273d6ee6425efdc41c2cc1987985cb6ac15e82
Jochen Hoenicke <hoenicke@gmail.com>
2020-05-28 10:55:59 +0200

    Prepare for competition
    * added final seed
    * tweaked filenames/directories
    * removed unsupported spaces from make_spaces
    * use wrapped solver ids

--- 2020/prep/selection/make_selection.sh
+++ 2020/prep/selection/make_selection.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-OUT_SQ=$1
-OUT_INC=$2
-OUT_MV=$3
-OUT_UC=$4
+mkdir -p final
+OUT_SQ="final/benchmark_selection_single_query"
+OUT_INC="final/benchmark_selection_incremental"
+OUT_MV="final/benchmark_selection_model_validation"
+OUT_UC="final/benchmark_selection_unsat_core"
 
 SCRIPTDIR=`dirname $(readlink -f "$0")`
 SELECT="$SCRIPTDIR/../../../tools/selection/selection.py"
@@ -15,8 +16,6 @@ FILTER_SQ_CSV_2019="$SCRIPTDIR/../../../2019/results/Single_Query_Track.csv"
 
 BENCHMARKS_INC="$SCRIPTDIR/../SMT-LIB_incremental_benchmarks_all_filtered.txt"
 NEW_INC_CSV="$SCRIPTDIR/../SMT-LIB_incremental_benchmarks_new_filtered.txt"
-FILTER_INC_CSV_2018="$SCRIPTDIR/../../../2018/csv/Application_Track.csv"
-FILTER_INC_CSV_2019="$SCRIPTDIR/../../../2019/results/Incremental_Track.csv"
 
 BENCHMARKS_UC="$SCRIPTDIR/../SMT-LIB_non_incremental_benchmarks_all.txt"
 NEW_UC_CSV="$SCRIPTDIR/../SMT-LIB_non_incremental_benchmarks_new.txt"
@@ -26,7 +25,7 @@ BENCHMARKS_MV="$SCRIPTDIR/../SMT-LIB_non_incremental_benchmarks_all.txt"
 NEW_MV_CSV="$SCRIPTDIR/../SMT-LIB_non_incremental_benchmarks_new.txt"
 FILTER_MV_CSV="$SCRIPTDIR/../SMT-LIB_non_incremental_benchmarks_all_assertions.csv"
 
-SEED=0
+SEED=332349782
 
 printf "MV TRACK\n\n"
 python $SELECT --benchmarks "$BENCHMARKS_MV" --new-benchmarks "$NEW_MV_CSV" -s $SEED --print-stats --out "$OUT_MV" --prefix "/non-incremental/" --logic "QF_BV;QF_LIA;QF_LRA;QF_LIRA;QF_RDL;QF_IDL" --sat "$FILTER_MV_CSV"
--- 2020/prep/submitted/make_spaces.sh
+++ 2020/prep/submitted/make_spaces.sh
@@ -15,40 +15,10 @@ IN_SPACE_NON_INC="../non-incremental-space.xml"
 IN_SPACE_INC="../incremental-space.xml"
 
 ### Benchmark selection
-SELECT_SQ="$SCRIPTDIR/../selection/single_query/benchmark_selection_single_query_2020_testjob2"
-SELECT_INC="$SCRIPTDIR/../selection/incremental/benchmark_selection_incremental_2020_testjob2"
-SELECT_MV="$SCRIPTDIR/../selection/model_validation/benchmark_selection_model_validation_2020_testjob2"
-SELECT_UC="$SCRIPTDIR/../selection/unsat_core/benchmark_selection_unsatcore_2020_testjob2"
-
-
-### Fixed solver versions (non-competitive)
-FIXED_SOLVERS_NON_COMPETITIVE_SQ="24281,24282"  # STP-mergesat-fixed,STP-portfolio-fixed
-FIXED_SOLVERS_NON_COMPETITIVE_INC="24478"       # CVC4
-
-### Fixed solver versions (competitive)
-FIXED_SOLVERS_COMPETITIVE_SQ="24492"   # Colibri had an issue with the with the
-                                       # original StarExec configuration file
-                                       # that broke solver wrapping
-
-### Best solvers 2018
-# Main track
-SOLVERS_2018_SQ="19771,19772,19775,19777,19783,19784,19788,19791,19792"
-# Application track
-SOLVERS_2018_INC="19991,19992,19993,19994,19995,19996"
-# Unsat core track
-SOLVERS_2018_UC="19793,19795,19796,19797,19827"
-
-### Non-competitive solvers
-# Z3,Boolector-ReasonLS,CVC4-SymBreak
-NON_COMPETITIVE_SOLVERS_SQ="24192,24193,24160,$SOLVERS_2018_SQ,$FIXED_SOLVERS_NON_COMPETITIVE_SQ"
-# Z3
-NON_COMPETITIVE_SOLVERS_CHALL_NON_INC="24192,$SOLVERS_2018_SQ"
-# Z3
-NON_COMPETITIVE_SOLVERS_CHALL_INC="24017,24478,$SOLVERS_2018_INC"
-# Z3,Boolector-ReasonLS
-NON_COMPETITIVE_SOLVERS_INC="24017,23970,$SOLVERS_2018_INC,$FIXED_SOLVERS_NON_COMPETITIVE_INC"
-# Z3
-NON_COMPETITIVE_SOLVERS_UC="24202,$SOLVERS_2018_UC"
+SELECT_SQ="$SCRIPTDIR/../selection/final/benchmark_selection_single_query"
+SELECT_INC="$SCRIPTDIR/../selection/final/benchmark_selection_incremental"
+SELECT_MV="$SCRIPTDIR/../selection/final/benchmark_selection_model_validation"
+SELECT_UC="$SCRIPTDIR/../selection/final/benchmark_selection_unsat_core"
 
 ### Output space xml files
 # Single Query
@@ -77,6 +47,11 @@ OUT_SPACE_UC_2018=
 # Model Validation Track
 OUT_SPACE_MV=
 
+# output usage if called without parameters
+if [ $# -eq 0 ]; then
+    set -- -h
+fi
+
 while [ $# -gt 0 ]
 do
   case $1 in
@@ -88,32 +63,8 @@ do
       echo "  options:"
       echo "    -h, --help           Print this message and exit"
       echo "    --sq          <file> Single Query track (no strings) output xml"
-      echo "    --sq-fixed    <file> Single Query track (no strings) output xml"
-      echo "                         for fixed (non-competing) solvers"
-      echo "    --sq-2018     <file> Single Query track (no strings) output xml"
-      echo "                         for best solvers 2018"
-      echo "    --sq-strings  <file> Single Query track (strings only) "
-      echo "                         output xml"
       echo "    --inc         <file> Incremental track output xml"
-      echo "    --inc-unknown <file> Incremental track for benchmarks with"
-      echo "                         unknown status output xml"
-      echo "    --inc-2018    <file> Incremental track output xml for best "
-      echo "                         solvers 2018"
-      echo "    --csq         <file> Challenge track non-incremental output xml"
-      echo "    --csq-fixed   <file> Challenge track non-incremental for fixed "
-      echo "                         competing solvers"
-      echo "    --csq-fixednc <file> Challenge track non-incremental for fixed "
-      echo "                         non-competing solvers"
-      echo "    --csq-2018    <file> Challenge track non-incremental output xml"
-      echo "                         for best solvers 2018"
-      echo "    --cinc        <file> Challenge track incremental output xml"
-      echo "    --cinc-fixed  <file> Challenge track incremental output xml"
-      echo "                         for fixed (non-competitive) solvers"
-      echo "    --cinc-2018   <file> Challenge track incremental output xml"
-      echo "                         for best solvers 2018"
       echo "    --uc          <file> Unsat Core track output xml"
-      echo "    --uc-2018     <file> Unsat Core track output xml for best "
-      echo "                         solvers 2018"
       echo "    --mv          <file> Model Validation track output xml"
       echo
       exit
@@ -121,18 +72,6 @@ do
     --sq)
       shift
       OUT_SPACE_SQ=$1
-      ;;
-    --sq-strings)
-      shift
-      OUT_SPACE_SQ_STRINGS=$1
-      ;;
-    --sq-fixed)
-      shift
-      OUT_SPACE_SQ_FIXED=$1
-      ;;
-    --sq-fixednc)
-      shift
-      OUT_SPACE_SQ_FIXED_NC=$1
       ;;
     --sq-2018)
       shift
@@ -142,45 +81,9 @@ do
       shift
       OUT_SPACE_INC=$1
       ;;
-    --inc-unknown)
-      shift
-      OUT_SPACE_INC_UNKNOWN=$1
-      ;;
-    --inc-fixed)
-      shift
-      OUT_SPACE_INC_FIXED=$1
-      ;;
     --inc-2018)
       shift
       OUT_SPACE_INC_2018=$1
-      ;;
-    --csq)
-      shift
-      OUT_SPACE_CHALL_NON_INC=$1
-      ;;
-    --csq-fixed)
-      shift
-      OUT_SPACE_CHALL_NON_INC_FIXED=$1
-      ;;
-    --csq-2018)
-      shift
-      OUT_SPACE_CHALL_NON_INC_2018=$1
-      ;;
-    --cinc)
-      shift
-      OUT_SPACE_CHALL_INC=$1
-      ;;
-    --cinc-fixed)
-      shift
-      OUT_SPACE_CHALL_INC_FIXED=$1
-      ;;
-    --cinc-unknown)
-      shift
-      OUT_SPACE_CHALL_INC_UNKNOWN=$1
-      ;;
-    --cinc-2018)
-      shift
-      OUT_SPACE_CHALL_INC_2018=$1
       ;;
     --uc)
       shift
@@ -206,7 +109,7 @@ done
 
 # Single Query Track
 [[ -n $OUT_SPACE_SQ ]] && \
-python $PREPARE "$IN_SPACE_NON_INC" "$SOLVERS_CSV" "$OUT_SPACE_SQ" -t single_query --select "$SELECT_SQ" -p
+python $PREPARE "$IN_SPACE_NON_INC" "$SOLVERS_CSV" "$OUT_SPACE_SQ" -t single_query --select "$SELECT_SQ" -w
 
 # Incremental Track
 [[ -n $OUT_SPACE_INC ]] && \
@@ -214,8 +117,8 @@ python $PREPARE "$IN_SPACE_INC" "$SOLVERS_CSV" "$OUT_SPACE_INC" -t incremental -
 
 # Model Validation Track
 [[ -n $OUT_SPACE_MV ]] && \
-python $PREPARE "$IN_SPACE_NON_INC" "$SOLVERS_CSV" "$OUT_SPACE_MV" -t model_validation --select "$SELECT_MV" -p
+python $PREPARE "$IN_SPACE_NON_INC" "$SOLVERS_CSV" "$OUT_SPACE_MV" -t model_validation --select "$SELECT_MV" -w
 
 # Unsat Core Track
 [[ -n $OUT_SPACE_UC ]] && \
-python $PREPARE "$IN_SPACE_NON_INC" "$SOLVERS_CSV" "$OUT_SPACE_UC" -t unsat_core --select "$SELECT_UC" -p
+python $PREPARE "$IN_SPACE_NON_INC" "$SOLVERS_CSV" "$OUT_SPACE_UC" -t unsat_core --select "$SELECT_UC" -w
