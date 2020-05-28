1944f71f446e25ffa731d94e6c31ac8cd69f1348
Jochen Hoenicke <hoenicke@gmail.com>
2020-05-28 22:35:53 +0200

    Fix typo

--- 2020/prep/selection/update_webpage.sh
+++ 2020/prep/selection/update_webpage.sh
@@ -16,4 +16,4 @@ mkdir -p $NCDIR
 
 ../../../tools/prep/extract_data_from_solvers_divisions.py -d $DIVISIONS -c ../../registration/canon_solvers.json -n $NCDIR ../../registration/solvers_divisions_final.csv $OUTPUT_SOLVERS $YEAR
 
-$MAKE_DIVISION -d $DIVISIONS -y $YEAR --single_query $BENCHMARKS_SQ --incremental $BENCHMARKS_SQ --model_validation $BENCHMARKS_MV --unsat_core $BENCHMARKS_UC --single_query-noncompetitive $NCDIR/single_query --incremental-noncompetitive $NCDIR/incremental --unsat_core-noncompetitive $NCDIR/unsat_core --model_validation-noncompetitive $NCDIR/model_validation $OUTPUT_DIVISIONS
+$MAKE_DIVISION -d $DIVISIONS -y $YEAR --single_query $BENCHMARKS_SQ --incremental $BENCHMARKS_INC --model_validation $BENCHMARKS_MV --unsat_core $BENCHMARKS_UC --single_query-noncompetitive $NCDIR/single_query --incremental-noncompetitive $NCDIR/incremental --unsat_core-noncompetitive $NCDIR/unsat_core --model_validation-noncompetitive $NCDIR/model_validation $OUTPUT_DIVISIONS
