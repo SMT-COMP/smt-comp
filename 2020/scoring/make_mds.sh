2074c605d18e6cdbd5fe6f1680a93be1a7187e92
Jochen Hoenicke <hoenicke@gmail.com>
2020-06-17 19:30:50 +0200

    Filter benchmarks from blacklist

--- 2020/scoring/make_mds.sh
+++ 2020/scoring/make_mds.sh
@@ -4,13 +4,19 @@ SCRIPTDIR=`dirname $(readlink -f "$0")`
 
 OUTPUT=results
 
+PYTHON=python3
 SCORE="$SCRIPTDIR/../../tools/scoring/score.py"
+FILTER="$SCRIPTDIR/../../tools/scoring/filter_result_csv.py"
 
 RESULTS_SQ="$SCRIPTDIR/../results/Single_Query_Track.csv"
 RESULTS_SQ_STRINGS="$SCRIPTDIR/../../2019/results/Single_Query_Track_strings.csv"
 RESULTS_INC="$SCRIPTDIR/../results/Incremental_Track.csv"
 RESULTS_UC="$SCRIPTDIR/../results/Unsat_Core_Track.csv"
 RESULTS_MV="$SCRIPTDIR/../results/Model_Validation_Track.csv"
+
+BLACKLIST_SQ="blacklist_nonincremental.txt"
+BLACKLIST_UC="blacklist_nonincremental.txt"
+BLACKLIST_MV="blacklist_nonincremental.txt"
 
 INC_NUM_CHECK_SAT="$SCRIPTDIR/../prep/SMT-LIB_incremental_benchmarks_num_check_sat.csv"
 
@@ -73,12 +79,13 @@ done
 mkdir -p $OUTPUT
 
 [[ -n $GEN_SQ ]] && \
-python3 $SCORE -y 2020 --csv $RESULTS_SQ  -t $TIME --gen-md $OUTPUT -T sq -S $SOLVERS_CSV
-[[ -n $GEN_SQ_STRINGS ]] && \
-python3 $SCORE -y 2020 --csv $RESULTS_SQ_STRINGS  -t $TIME --gen-md $OUTPUT -T sq -S $SOLVERS_CSV
+$PYTHON $FILTER $RESULTS_SQ --exclude $BLACKLIST_SQ > sq-filtered.csv && \
+$PYTHON $SCORE -y 2020 --csv sq-filtered.csv  -t $TIME --gen-md $OUTPUT -T sq -S $SOLVERS_CSV
 [[ -n $GEN_INC ]] && \
-python3 $SCORE -y 2020 --csv $RESULTS_INC  -t $TIME --gen-md $OUTPUT -T inc -S $SOLVERS_CSV -i $INC_NUM_CHECK_SAT
+$PYTHON $SCORE -y 2020 --csv $RESULTS_INC  -t $TIME --gen-md $OUTPUT -T inc -S $SOLVERS_CSV -i $INC_NUM_CHECK_SAT
 [[ -n $GEN_UC ]] && \
-python3 $SCORE -y 2020 --csv $RESULTS_UC  -t $TIME --gen-md $OUTPUT -T uc -S $SOLVERS_CSV
+$PYTHON $FILTER $RESULTS_UC --exclude $BLACKLIST_UC > uc-filtered.csv && \
+$PYTHON $SCORE -y 2020 --csv uc-filtered.csv  -t $TIME --gen-md $OUTPUT -T uc -S $SOLVERS_CSV
 [[ -n $GEN_MV ]] && \
-python3 $SCORE -y 2020 --csv $RESULTS_MV  -t $TIME --gen-md $OUTPUT -T mv -S $SOLVERS_CSV
+$PYTHON $FILTER $RESULTS_MV --exclude $BLACKLIST_MV > mv-filtered.csv && \
+$PYTHON $SCORE -y 2020 --csv mv-filtered.csv  -t $TIME --gen-md $OUTPUT -T mv -S $SOLVERS_CSV
