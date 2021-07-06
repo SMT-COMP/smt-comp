e9a2dd77dcc5b157a45a7a774023769adb7239ba
Jochen Hoenicke <hoenicke@gmail.com>
2021-07-06 19:05:57 +0200

    added script to download and merge results

---
+++ 2021/results/join.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Download Jobs from StarExec
+DOWNLOAD=0
+
+# Jobs in the order "competition best-of non-competing"
+Single_Query="47304 47543 47546"
+Incremental="47350 47540 47689"
+Model_Validation="47305 47542 47727"
+
+for track in Single_Query Incremental Model_Validation; do
+    OUTPUT="${track}_Track.csv"
+    rm -f $OUTPUT
+    for id in ${!track}; do
+	if [ "$DOWNLOAD" == "1" ]; then
+	    wget -OJob${id}_info.zip "https://www.starexec.org/starexec/secure/download?type=job&id=${id}&returnids=true&getcompleted=false"
+	    unzip Job${id}_info.zip
+	fi
+	INPUT="Job${id}/Job${id}_info.csv"
+        if [ -e $OUTPUT ]; then
+	    # sanity check: head must be equal
+	    diff <(head -1 $INPUT) <(head -1 $OUTPUT)
+	    tail -n +1 $INPUT >> $OUTPUT
+	else
+	    cat $INPUT > $OUTPUT
+	fi
+    done
+done
