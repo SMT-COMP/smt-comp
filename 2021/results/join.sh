694ade837b21c805f74b1c83b7632b55fe8f8e45
Jochen Hoenicke <hoenicke@gmail.com>
2021-07-13 17:40:46 +0200

    updated join script to merge unsat core track correctly

--- 2021/results/join.sh
+++ 2021/results/join.sh
@@ -9,7 +9,10 @@ Incremental="47350 47540 47689"
 Model_Validation="47305 47542 47727"
 Unsat_Core="47790 47815 47866 47867"
 
+COLORDER=../../tools/process-results/unify_column_order.py
+
 for track in Single_Query Incremental Model_Validation Unsat_Core; do
+    echo -n "$track:"
     OUTPUT="${track}_Track.csv"
     rm -f $OUTPUT
     for id in ${!track}; do
@@ -18,12 +21,26 @@ for track in Single_Query Incremental Model_Validation Unsat_Core; do
             unzip Job${id}_info.zip
         fi
         INPUT="Job${id}/Job${id}_info.csv"
+        echo -n " Job${id}"
         if [ -e $OUTPUT ]; then
-            # sanity check: head must be equal
-            diff <(head -1 $INPUT) <(head -1 $OUTPUT)
-            tail -n +2 $INPUT >> $OUTPUT
+            TMPFILE=$(mktemp --suffix=.csv)
+            $COLORDER -o $OUTPUT -a $INPUT > $TMPFILE
+
+            # For rerun jobs, remove one `/` to make all benchmark
+            # paths on the same level
+            FILTER=
+            if [ "$id" == "47866" -o $id == "47867" ]; then
+                FILTER="| sed -e 's!Unsat Core Rerun/!Unsat Core Rerun !'"
+            fi
+
+            # sanity check: head must be equal now
+            diff <(head -1 $TMPFILE) <(head -1 $OUTPUT)
+            eval tail -n +2 $TMPFILE $FILTER >> $OUTPUT
+            rm $TMPFILE
         else
-            cat $INPUT > $OUTPUT
+            # convert all files to same line ending
+            dos2unix -q -n $INPUT $OUTPUT
         fi
     done
+    echo
 done
