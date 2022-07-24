0458272eb0849d8765c94c611f6ca6b325dbf2c9
Jochen Hoenicke <hoenicke@gmail.com>
2022-07-24 13:15:31 +0200

    Selections for mv, uc, and pe tracks

--- 2022/prep/selection/update_webpage.sh
+++ 2022/prep/selection/update_webpage.sh
@@ -4,9 +4,9 @@ YEAR=2022
 DIVISIONS=../../new-divisions.json
 BENCHMARKS_SQ=final/benchmark_selection_single_query
 BENCHMARKS_INC=final/benchmark_selection_incremental
-#BENCHMARKS_MV=final/benchmark_selection_model_validation
-#BENCHMARKS_UC=final/benchmark_selection_unsat_core
-#BENCHMARKS_PE=final/benchmark_selection_proof_exhibition
+BENCHMARKS_MV=final/benchmark_selection_model_validation
+BENCHMARKS_UC=final/benchmark_selection_unsat_core
+BENCHMARKS_PE=final/benchmark_selection_proof_exhibition
 MAKE_DIVISION=../../../tools/selection/prepare_benchmark_ymls.py
 OUTPUT_SOLVERS=../../../../smt-comp.github.io/_participants_$YEAR/
 OUTPUT_DIVISIONS=../../../../smt-comp.github.io/_divisions_$YEAR/
@@ -20,5 +20,4 @@ EXPERIMENTAL_CSV=../../experimental.csv
 #
 #$MAKE_DIVISION -d $DIVISIONS -y $YEAR --single_query $BENCHMARKS_SQ --incremental $BENCHMARKS_INC --model_validation $BENCHMARKS_MV --unsat_core $BENCHMARKS_UC --single_query-noncompetitive $NCDIR/single_query --incremental-noncompetitive $NCDIR/incremental --unsat_core-noncompetitive $NCDIR/unsat_core --model_validation-noncompetitive $NCDIR/model_validation $OUTPUT_DIVISIONS --experimental $EXPERIMENTAL_CSV
 
-$MAKE_DIVISION -d $DIVISIONS -y $YEAR --single_query $BENCHMARKS_SQ --incremental $BENCHMARKS_INC --experimental $EXPERIMENTAL_CSV $OUTPUT_DIVISIONS
-# --model_validation $BENCHMARKS_MV --unsat_core $BENCHMARKS_UC --proof_exhibition $BENCHMARKS_PE
+$MAKE_DIVISION -d $DIVISIONS -y $YEAR --single_query $BENCHMARKS_SQ --incremental $BENCHMARKS_INC --model_validation $BENCHMARKS_MV --unsat_core $BENCHMARKS_UC --proof_exhibition $BENCHMARKS_PE --experimental $EXPERIMENTAL_CSV $OUTPUT_DIVISIONS
