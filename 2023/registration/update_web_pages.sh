d44ffbb2d2937d52bb6ada646c9e0e2d4475e629
Jochen Hoenicke <hoenicke@gmail.com>
2023-05-30 16:27:43 +0200

    Set final ids, NYSE open value

--- 2023/registration/update_web_pages.sh
+++ 2023/registration/update_web_pages.sh
@@ -6,7 +6,7 @@
 
 YEAR=2023
 NYSE_DATE=2023-05-30
-NYSE_VALUE=""  # NYSE opening value, e.g., 12345.6789
+NYSE_VALUE="15078.6900"  # NYSE opening value, e.g., 12345.6789
 LOGICS="../divisions.json"
 DIVISIONS="../new-divisions.json"
 
