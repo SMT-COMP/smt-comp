56aadac11e1e52d0099a2ec530e8a0a9d7e986be
François Bobot <francois.bobot@cea.fr>
2023-06-08 17:19:41 +0200

    find-disagreement with not sound solvers

--- 2023/results/find-disagreements.sh
+++ 2023/results/find-disagreements.sh
@@ -2,7 +2,7 @@
 
 #benchmark,solver,configuration,status,cpu time,wallclock time,memory usage,result,expected
 
-cat raw-results-sq.csv |cut -d, -f 1,8 | grep -v -e starexec-unknown -e '--' | sort |uniq | cut -d, -f1 |uniq -c |grep -v ' 1 '  | cut -c 9- > disagreeing-sq.txt
+cat raw-results-sq.csv | cut -d, -f 1,8 | grep -v -e starexec-unknown -e '--' | sort |uniq | cut -d, -f1 |uniq -c |grep -v ' 1 '  | cut -c 9- > disagreeing-sq.txt
 
 COMMA=""
 PATTERN=""
@@ -11,4 +11,8 @@ while read bench; do
     COMMA="|"
 done < disagreeing-sq.txt
 # remove the non informative result and select the problematic files
-cat raw-results-sq.csv | grep -v -e ",starexec-unknown," | grep -E "($PATTERN)," | LANG=C sort -t, -k 1
+cat raw-results-sq.csv | grep -v -e ",starexec-unknown," | grep -E "^($PATTERN)," | LANG=C sort -t, -k 1 | cut -d, -f 2,8
+
+
+# Solver with answers different from the others
+#| grep -v -E "vampire_4.8_smt_pre|iProver-3.8-Final|z3-Owl-Final|OSTRICH 1.3 SMT-COMP|UltimateIntBlastingWrapper\+SMTInterpol|Z3-Noodler"
